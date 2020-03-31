41d8dfb1fabc1409cb277206f255496d5056a740
dmitrykirilin <demonus18@gmail.com>
2020-04-01 00:47:10 +0300

    Добавлен Graph

---
+++ lab3/painting.py
@@ -0,0 +1,15 @@
+from graph import *
+
+windowSize(500, 500)
+penColor(0,0,0)
+penSize(2)
+brushColor("yellow")
+
+circle(250, 250, 150)
+
+brushColor("black")
+rectangle(250-60, 250+50, 250+60, 250+80)
+
+polygon([(30, 30), (230, 200), (210, 220), (10, 50), (30, 30)])
+
+run()
